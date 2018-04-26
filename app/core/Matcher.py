ab11d13dbb7bcb9f4fc8517f718bf8f49fecb29e
elgenius <abdoolly@gmail.com>
2018-04-27 00:45:07 +0200

    added the matcher class which is responsible for matching words

---
+++ app/core/Matcher.py
@@ -0,0 +1,15 @@
+class Matcher:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def matchWord(word: str, wordsList: list):
+        # this is an initialization of the current class so you can create other functions in the class and use it as
+        #  you wish
+
+        this = Matcher()
+
+        """
+        matching implementation should be done here
+        """
+        return
